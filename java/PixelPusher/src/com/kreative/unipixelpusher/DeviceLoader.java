d0da8bf06500360091032d15f64feab93c237022
Rebecca G. Bettencourt <beckiejo@gmail.com>
2018-06-18 11:31:42 -0700

    a bit of refactoring DeviceLoader

--- java/PixelPusher/src/com/kreative/unipixelpusher/DeviceLoader.java
+++ java/PixelPusher/src/com/kreative/unipixelpusher/DeviceLoader.java
@@ -10,19 +10,25 @@ import java.util.Map;
 import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class DeviceLoader {
 	private final Set<PixelDeviceRegistry> registries;
-	private final Map<PixelDevice,String> deviceNameMap;
-	private final SortedMap<String,PixelDevice> nameDeviceMap;
+	private final SortedSet<DeviceInfo> deviceSet;
+	private final SortedMap<String,DeviceInfo> idMap;
+	private final SortedMap<String,DeviceInfo> nameMap;
+	private final Map<PixelDevice,DeviceInfo> deviceMap;
 	private final List<PixelDeviceListener> listeners;
 	private final MyListener myListener;
 	
 	public DeviceLoader(DeviceConfiguration config) {
 		this.registries = Collections.synchronizedSet(new HashSet<PixelDeviceRegistry>());
-		this.deviceNameMap = Collections.synchronizedMap(new HashMap<PixelDevice,String>());
-		this.nameDeviceMap = Collections.synchronizedSortedMap(new TreeMap<String,PixelDevice>());
+		this.deviceSet = Collections.synchronizedSortedSet(new TreeSet<DeviceInfo>());
+		this.idMap = Collections.synchronizedSortedMap(new TreeMap<String,DeviceInfo>());
+		this.nameMap = Collections.synchronizedSortedMap(new TreeMap<String,DeviceInfo>());
+		this.deviceMap = Collections.synchronizedMap(new HashMap<PixelDevice,DeviceInfo>());
 		this.listeners = Collections.synchronizedList(new ArrayList<PixelDeviceListener>());
 		this.myListener = new MyListener();
 		
@@ -30,24 +36,32 @@ public class DeviceLoader {
 			PixelDeviceRegistry r = f.createInstance(config);
 			registries.add(r);
 			for (PixelDevice d : r.getDevices()) {
-				deviceNameMap.put(d, d.name());
-				nameDeviceMap.put(d.name(), d);
+				DeviceInfo info = new DeviceInfo(d);
+				add(info);
 			}
 			r.addPixelDeviceListener(myListener);
 			r.update();
 		}
 	}
 	
-	public Collection<PixelDevice> getDevices() {
-		return Collections.unmodifiableCollection(deviceNameMap.keySet());
+	public Collection<DeviceInfo> getDevices() {
+		return Collections.unmodifiableCollection(deviceSet);
+	}
+	
+	public Collection<String> getDeviceIds() {
+		return Collections.unmodifiableCollection(idMap.keySet());
 	}
 	
 	public Collection<String> getDeviceNames() {
-		return Collections.unmodifiableCollection(nameDeviceMap.keySet());
+		return Collections.unmodifiableCollection(nameMap.keySet());
 	}
 	
-	public PixelDevice getDevice(String name) {
-		return nameDeviceMap.get(name);
+	public DeviceInfo getDeviceById(String id) {
+		return idMap.get(id);
+	}
+	
+	public DeviceInfo getDeviceByName(String name) {
+		return nameMap.get(name);
 	}
 	
 	public void addPixelDeviceListener(PixelDeviceListener listener) {
@@ -62,32 +76,102 @@ public class DeviceLoader {
 		return listeners.toArray(new PixelDeviceListener[listeners.size()]);
 	}
 	
+	private synchronized void add(DeviceInfo info) {
+		deviceSet.add(info);
+		idMap.put(info.id, info);
+		nameMap.put(info.name, info);
+		deviceMap.put(info.device, info);
+	}
+	
+	private synchronized void remove(DeviceInfo info) {
+		deviceSet.remove(info);
+		idMap.remove(info.id);
+		nameMap.remove(info.name);
+		deviceMap.remove(info.device);
+	}
+	
 	private class MyListener implements PixelDeviceListener {
 		@Override
 		public synchronized void pixelDeviceAppeared(PixelDevice dev) {
-			deviceNameMap.put(dev, dev.name());
-			nameDeviceMap.put(dev.name(), dev);
+			DeviceInfo info = deviceMap.get(dev);
+			if (info != null) { remove(info); info.update(dev); }
+			else { info = new DeviceInfo(dev); }
+			add(info);
 			for (PixelDeviceListener listener : listeners) {
 				listener.pixelDeviceAppeared(dev);
 			}
 		}
 		@Override
 		public synchronized void pixelDeviceChanged(PixelDevice dev) {
-			String name = deviceNameMap.remove(dev);
-			if (name != null) nameDeviceMap.remove(name);
-			deviceNameMap.put(dev, dev.name());
-			nameDeviceMap.put(dev.name(), dev);
+			DeviceInfo info = deviceMap.get(dev);
+			if (info != null) { remove(info); info.update(dev); }
+			else { info = new DeviceInfo(dev); }
+			add(info);
 			for (PixelDeviceListener listener : listeners) {
 				listener.pixelDeviceChanged(dev);
 			}
 		}
 		@Override
 		public synchronized void pixelDeviceDisappeared(PixelDevice dev) {
-			String name = deviceNameMap.remove(dev);
-			if (name != null) nameDeviceMap.remove(name);
+			DeviceInfo info = deviceMap.get(dev);
+			if (info != null) { remove(info); }
 			for (PixelDeviceListener listener : listeners) {
 				listener.pixelDeviceDisappeared(dev);
 			}
 		}
+	}
+	
+	public static final class DeviceInfo implements Comparable<DeviceInfo> {
+		private String id;
+		private String name;
+		private PixelDevice device;
+		
+		private DeviceInfo(PixelDevice dev) {
+			this.id = dev.id();
+			this.name = dev.name();
+			this.device = dev;
+		}
+		
+		private void update(PixelDevice dev) {
+			this.id = dev.id();
+			this.name = dev.name();
+			this.device = dev;
+		}
+		
+		public String id() { return id; }
+		public String name() { return name; }
+		public PixelDevice device() { return device; }
+		
+		@Override
+		public int compareTo(DeviceInfo that) {
+			return this.name.compareToIgnoreCase(that.name);
+		}
+		
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
+	
+	public static void main(String[] args) {
+		DeviceConfiguration config = new DeviceConfiguration();
+		DeviceLoader loader = new DeviceLoader(config);
+		for (String name : loader.getDeviceNames()) {
+			System.out.println("  " + name);
+		}
+		loader.addPixelDeviceListener(new PixelDeviceListener() {
+			@Override
+			public void pixelDeviceAppeared(PixelDevice dev) {
+				System.out.println("+ " + dev.name());
+			}
+			@Override
+			public void pixelDeviceChanged(PixelDevice dev) {
+				System.out.println("* " + dev.name());
+			}
+			@Override
+			public void pixelDeviceDisappeared(PixelDevice dev) {
+				System.out.println("- " + dev.name());
+			}
+		});
 	}
 }
