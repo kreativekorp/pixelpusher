5ee22da62bc36498d08fa5e8eea8d7a0e943b06a
Rebecca G. Bettencourt <beckiejo@gmail.com>
2018-06-24 00:46:16 -0700

    thread pool

---
+++ java/PixelPusher/src/com/kreative/unipixelpusher/PusherThreadPool.java
@@ -0,0 +1,63 @@
+package com.kreative.unipixelpusher;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PusherThreadPool {
+	private Map<PixelSequence,PusherThread> sequenceThreads;
+	private Map<PixelString,PusherThread> stringThreads;
+	private Set<PusherThread> threads;
+	
+	public PusherThreadPool() {
+		this.sequenceThreads = new HashMap<PixelSequence,PusherThread>();
+		this.stringThreads = new HashMap<PixelString,PusherThread>();
+		this.threads = new HashSet<PusherThread>();
+	}
+	
+	public synchronized void connect(PixelSequence sequence, PixelString string) {
+		disconnect(sequence);
+		disconnect(string);
+		PusherThread thread = new PusherThread(sequence, string);
+		sequenceThreads.put(sequence, thread);
+		stringThreads.put(string, thread);
+		threads.add(thread);
+		thread.start();
+	}
+	
+	public synchronized void disconnect(PixelSequence sequence, PixelString string) {
+		disconnect(sequence);
+		disconnect(string);
+	}
+	
+	public synchronized void disconnect(PixelSequence sequence) {
+		PusherThread thread = sequenceThreads.remove(sequence);
+		if (thread != null) {
+			stringThreads.remove(thread.string);
+			threads.remove(thread);
+			thread.interrupt();
+		}
+	}
+	
+	public synchronized void disconnect(PixelString string) {
+		PusherThread thread = stringThreads.remove(string);
+		if (thread != null) {
+			sequenceThreads.remove(thread.sequence);
+			threads.remove(thread);
+			thread.interrupt();
+		}
+	}
+	
+	public synchronized void disconnectAll() {
+		for (PusherThread thread : threads) thread.interrupt();
+		this.sequenceThreads = new HashMap<PixelSequence,PusherThread>();
+		this.stringThreads = new HashMap<PixelString,PusherThread>();
+		this.threads = new HashSet<PusherThread>();
+	}
+	
+	public synchronized Set<PusherThread> threadSet() {
+		return Collections.unmodifiableSet(new HashSet<PusherThread>(threads));
+	}
+}
